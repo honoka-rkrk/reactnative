8ee41d59be063d59e0fd27627a5fcd99f4577808
HONOKA ANRAKU <rakuraku2563nonnon@gmail.com>
2021-11-26 10:07:03 +0900

    Create LoginScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -5,6 +5,8 @@ import MemoListScreen from "./src/screens/MemoListScreen";
 import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 import MemoEditScreen from "./src/screens/MemoEditScree";
 import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
 export default function App() {
-  return <MemoCreateScreen />;
+  return <SignUpScreen />;
 }
