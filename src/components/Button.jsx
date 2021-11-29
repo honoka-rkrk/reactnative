e61a26b6a051ad7fa7af58bfabc239295d79ea9b
HONOKA ANRAKU <rakuraku2563nonnon@gmail.com>
2021-11-29 09:50:56 +0900

    Create Loading component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable */
 import React from 'react';
 import {View,Text,StyleSheet,TouchableOpacity} from 'React-native';
-import {string,func} from 'prop-types';
+import {string,func,shape} from 'prop-types';
 
 export default function Button(props){
-    const {label,onPress} = props;
+    const {label,onPress,style} = props;
     return(
-        <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
+        <TouchableOpacity style={[styles.buttonContainer,style]} onPress={onPress}>
             <Text style={styles.buttonLabel}>{label}</Text>
         </TouchableOpacity>
     );
@@ -15,11 +15,13 @@ export default function Button(props){
 Button.propTypes = {
     label:string.isRequired,
     onPress:func,
+    style:shape(),
 };
 
 Button.defaultProps = {
     onPress :null,
-}
+    style:null,
+};
 
 const styles = StyleSheet.create({
     buttonContainer:{
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -3,11 +3,13 @@ import React,{useState,useEffect} from 'react';
 import {View,Text,TextInput,StyleSheet,TouchableOpacity,Alert} from 'react-native';
 import firebase from 'firebase';
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function LoginScreen(props){
     const {navigation} = props;
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+    const [isLoading,setIsLoading] = useState(true);
 
     useEffect(() => {
         const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -16,12 +18,15 @@ export default function LoginScreen(props){
                     index:0,
                     routes:[{name:"MemoList"}],
                 });
+            }else{
+                setIsLoading(false);
             }
         });
         return unsubscribe;
     },[]);
 
     function handlePress(){
+        setIsLoading(true);
         firebase.auth().signInWithEmailAndPassword(email,password)
         .then((userCredential) => {
             const {user} = userCredential;
@@ -33,11 +38,15 @@ export default function LoginScreen(props){
         })
         .catch((error) => {
             Alert.alert(error.code);
+        })
+        .then(() => {
+            setIsLoading(false);
         });
     }
 
     return (
         <View style={styles.container}>
+            <Loading isLoading={isLoading} />
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
                 <TextInput
